4adaea7185dcfdf1d4d14c024b88f481e0243938
Conroy Cheers <conroy@kiki.local>
2025-11-29 21:47:49 +1100

    feat: init new darwin host "kiki"

---
+++ hosts/nix-darwin/kiki/configuration.nix
@@ -0,0 +1,67 @@
+{
+  inputs,
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+
+{
+  environment.systemPackages = with pkgs; [ ];
+
+  networking.hostName = "kiki";
+
+  users.users.conroy = {
+    description = "Conroy Cheers";
+    home = "/Users/conroy";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKvtQAUGvh3UmjM7blBM86VItgYD+22HYKzCBrXDsFGB" # conroy
+    ];
+    shell = pkgs.zsh;
+  };
+
+  andromeda = {
+    development.enable = true;
+  };
+
+  corncheese = {
+    system.enable = true;
+    brew.enable = true;
+    theming = {
+      enable = true;
+      theme = "catppuccin";
+    };
+    development = {
+      enable = true;
+      remoteBuilders.enable = true;
+    };
+  };
+
+  nixpkgs = {
+    overlays = [
+      # If you want to use overlays your own flake exports (from overlays dir):
+      inputs.self.overlays.karabiner
+    ];
+  };
+
+  programs.zsh.enable = true;
+
+  # Fonts
+  fonts.packages = with pkgs; [ nerd-fonts.meslo-lg ];
+
+  services = {
+    karabiner-elements = {
+      enable = false;
+    };
+  };
+
+  # Keyboard
+  system.keyboard.enableKeyMapping = true;
+  system.keyboard.remapCapsLockToEscape = false;
+
+  system.primaryUser = "conroy";
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # > darwin-rebuild changelog
+  system.stateVersion = 5;
+}
---
+++ hosts/nix-darwin/kiki/home/conroy.nix
@@ -0,0 +1,132 @@
+{
+  inputs,
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+
+{
+  imports = [ ];
+
+  config = {
+    home = {
+      username = "conroy";
+      homeDirectory = "/Users/conroy";
+      stateVersion = "25.11";
+    };
+
+    # Let Home Manager install and manage itself.
+    programs.home-manager.enable = true;
+
+    age.rekey = {
+      hostPubkey = lib.mkForce "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPiqaH6fb+jnt0wz/s67UARLes+tvvHbVCUC29gYEClC conroy@kiki.local";
+    };
+
+    # log conroy into atuin sync
+    age.secrets."corncheese.atuin.key" = {
+      rekeyFile = lib.repoSecret "corncheese/atuin/key.age";
+    };
+
+    corncheese = {
+      macos = {
+        enable = true;
+      };
+      development = {
+        vscode.enable = true;
+        ssh.enable = true;
+      };
+      theming = {
+        enable = true;
+        theme = "catppuccin";
+        themeOverrides = {
+          opacity = lib.mkForce 0.87;
+        };
+      };
+      desktop = {
+        enable = true;
+        firefox.enable = true;
+        element.enable = true;
+      };
+      shell = {
+        enable = true;
+        atuin = {
+          enable = true;
+          sync = true;
+          key = config.age.secrets."corncheese.atuin.key".path;
+        };
+        direnv = true;
+        zoxide = true;
+        shells = [ "zsh" ];
+      };
+      wezterm = {
+        enable = false;
+      };
+    };
+    andromeda = {
+      development.enable = true;
+    };
+
+    home.packages = with pkgs; [
+      # Nix
+      nil
+      nixfmt-rfc-style
+
+      teams
+    ];
+
+    programs.btop = {
+      enable = true;
+    };
+
+    programs.git = {
+      enable = true;
+      lfs.enable = true;
+      userName = "Conroy Cheers";
+      userEmail = "conroy@corncheese.org";
+      delta = {
+        enable = true;
+      };
+    };
+
+    programs.kitty = {
+      enable = true;
+      settings = {
+        scrollback_lines = 20000;
+        background_blur = 64;
+      };
+    };
+
+    programs.vscode.profiles.default.userSettings =
+      let
+        ptToPx = 1.0;
+      in
+      with config.stylix.fonts;
+      {
+        "editor.fontSize" = lib.mkForce (builtins.floor (sizes.terminal * ptToPx + 0.5));
+        "debug.console.fontSize" = lib.mkForce (builtins.floor (sizes.terminal * ptToPx + 0.5));
+        "markdown.preview.fontSize" = lib.mkForce (builtins.floor (sizes.terminal * ptToPx + 0.5));
+        "terminal.integrated.fontSize" = lib.mkForce (builtins.floor (sizes.terminal * ptToPx + 0.5));
+        "chat.editor.fontSize" = lib.mkForce (builtins.floor (sizes.terminal * ptToPx + 0.5));
+        "scm.inputFontSize" = lib.mkForce (builtins.floor (sizes.terminal * ptToPx * 13 / 14 + 0.5));
+      };
+
+    # services.gpg-agent = {
+    #   enable = true;
+    #   defaultCacheTtl = 1800;
+    #   enableSshSupport = true;
+    # };
+
+    # home.file.".stack/config.yaml".text = lib.generators.toYAML {} {
+    #   templates = {
+    #     scm-init = "git";
+    #     params = with config.programs.git; {
+    #       author-name = userName;
+    #       author-email = userEmail;
+    #       github-username = userName;
+    #     };
+    #   };
+    #   nix.enable = true;
+    # };
+  };
+}
