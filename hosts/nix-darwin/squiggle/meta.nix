62085d6c2b95244665d674232c9ab7e999eeb5d9
Conroy Cheers <conroy@corncheese.org>
2026-03-09 15:14:28 +1100

    feat!: drop squiggle

--- hosts/nix-darwin/squiggle/meta.nix
+++
@@ -1,7 +0,0 @@
-{
-  system = "aarch64-darwin";
-  nixpkgs.variant = "default";
-  enable = true;
-  hostname = "squiggle";
-  pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOf2Md7CBRi10aLHv+YnpCK5qP6RRNE0DJGuUx+vRLlC";
-}
