344376815d6aeedff8b8ca4bcc0c7186a084ce5a
Conroy Cheers <conroy@corncheese.org>
2026-03-11 08:32:01 +1100

    feat(ci): allow secret-less runs for PRs

---
+++ .github/ci-stubs/corncheese-server-config/flake.nix
@@ -0,0 +1,12 @@
+{
+  description = "CI stub for corncheese-server-config";
+
+  outputs = { ... }: {
+    nixosModules.corncheese-server = { lib, ... }: {
+      options.corncheese-server = lib.mkOption {
+        type = lib.types.attrs;
+        default = { };
+      };
+    };
+  };
+}
