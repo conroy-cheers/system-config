98cb526f4645dff65e64885054f9a9a60d17e1b6
Conroy Cheers <conroy@dromeda.com.au>
2026-05-11 09:07:59 +1000

    Add CJK font fallback

--- modules/nixos/corncheese-wm/common/fonts.nix
+++ modules/nixos/corncheese-wm/common/fonts.nix
@@ -11,6 +11,27 @@ let
 in
 {
   config = lib.mkIf cfg.enable {
-    fonts.packages = with pkgs; [ themeDetails.fontPkg ];
+    fonts = {
+      packages = with pkgs; [
+        themeDetails.fontPkg
+        noto-fonts-cjk-sans
+        noto-fonts-cjk-serif
+      ];
+
+      fontconfig.defaultFonts = {
+        sansSerif = [
+          "Noto Sans CJK SC"
+          "DejaVu Sans"
+        ];
+        serif = [
+          "Noto Serif CJK SC"
+          "DejaVu Serif"
+        ];
+        monospace = [
+          "Noto Sans Mono CJK SC"
+          "DejaVu Sans Mono"
+        ];
+      };
+    };
   };
 }
