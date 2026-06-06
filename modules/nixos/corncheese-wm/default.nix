1b9dd008c5136c3e52fa0f7e6ad84c7f6257b4a9
Conroy Cheers <conroy@corncheese.org>
2026-06-06 12:35:52 +1000

    feat(wm): add heroic-launcher

--- modules/nixos/corncheese-wm/default.nix
+++ modules/nixos/corncheese-wm/default.nix
@@ -170,7 +170,16 @@ in
               ];
           };
         };
+        programs.gamescope.enable = true;
+        programs.gamemode.enable = true;
         environment.systemPackages = with pkgs; [
+          (heroic.override {
+            extraPkgs =
+              pkgs': with pkgs'; [
+                gamescope
+                gamemode
+              ];
+          })
           steamtinkerlaunch
           prismlauncher
         ];
